b5af2dd8546831bad6bdc161ef5098a8f0d707a9
articnine9 <articnine9@gmail.com>
2024-09-04 15:28:14 +0530

    Update menu.js

--- backend/Routes/menu.js
+++ backend/Routes/menu.js
@@ -16,11 +16,43 @@ menu.get("/stocks", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+menu.post("/edit", async (req, res) => {
+  const { _id, name, stock, price, type, categoryName } = req.body;
 
+  if (
+    !ObjectId.isValid(_id) ||
+    !name ||
+    !stock ||
+    !price ||
+    !type ||
+    !categoryName
+  ) {
+    return res.status(400).json({ message: "Invalid input data" });
+  }
+
+  try {
+    const database = await db.getDatabase();
+    const collection = database.collection("menu");
+
+    const result = await collection.updateOne(
+      { _id: new ObjectId(_id) },
+      { $set: { name, stock, price, type, categoryName } }
+    );
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    res.status(200).json({ message: "Item updated successfully" });
+  } catch (error) {
+    console.error("Error updating item:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 menu.post("/add", async (req, res) => {
   try {
     const { name, stock, price, type, categoryName, availability } = req.body;
-    if (!name || !stock || !price || !type  || !categoryName || !availability) {
+    if (!name || !stock || !price || !type || !categoryName || !availability) {
       return res.status(400).json({ message: "All fields are required" });
     }
     let database = await db.getDatabase();
