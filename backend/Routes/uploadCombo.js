36a59dde2b2d4ea6625a7534542081af9b63b606
articnine9 <articnine9@gmail.com>
2025-03-04 11:17:10 +0530

    Update uploadCombo.js

--- backend/Routes/uploadCombo.js
+++ backend/Routes/uploadCombo.js
@@ -81,7 +81,6 @@ comboRouter.post("/add", upload.single("comboImage"), async (req, res) => {
           filename: req.file.originalname,
           contentType: req.file.mimetype,
           uploadDate: new Date(),
-            availability: req.body.availability || "available",
         };
 
         // Insert the combo data into the 'combos' collection
